b8a5817efe3e0c245c8add30e30a5c4297bacb6f
Karan Gandhi <gandhik199@gmail.com>
2025-01-26 20:52:33 +0530

    Enum and structure

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,10 +1,18 @@
 #include <QCoreApplication>
 #include <QDebug>
+enum RGB {red,green,blue=10};
+
+struct product {
+    int weight;
+    double value;
+    RGB colors;
+};
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    // variables and data types
     bool LightSwitch = false;
 
     qInfo() << "Is on:" <<LightSwitch;
@@ -22,9 +30,25 @@ int main(int argc, char *argv[])
     age = 39.2;
     qInfo()<<"Implicit conversion from double to int. Age:"<<age;
 
+    // signed and unsigned variables
     signed int dogs = -1;
     qInfo()<<"Dogs:"<<dogs;
     unsigned int cats = -10;
     qInfo()<<"Out of bounds value. Cats:"<<cats;
+
+    // enum
+    enum Color {red,green,blue=10};
+    Color objname = Color::blue;
+    qInfo()<<objname;
+
+    //structure
+    product laptop; // no assignment.
+    laptop.weight = 90;
+    laptop.value = 10079;
+    laptop.colors = RGB::green;
+
+    qInfo()<<laptop.weight;
+    qInfo()<<laptop.value;
+    qInfo()<<laptop.colors;
     return a.exec();
 }
