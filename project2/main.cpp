0aa94d10cc7e1867d9b8531e2ba389d6b2d0fb6f
Karan Gandhi <gandhik199@gmail.com>
2025-01-29 14:42:00 +0530

    switch case

--- project2/main.cpp
+++ project2/main.cpp
@@ -11,6 +11,19 @@ int main(int argc, char *argv[])
     qInfo()<<"Enter age:";
     cin >> age;
 
-    age>0 && age<101? qInfo("You are alive"): qFatal("Something wrong with your age");
+    age>=0 && age<101? qInfo("You are alive"): qFatal("Something wrong with your age");
+
+    //switch
+    switch (age) {
+    case 0:
+        qInfo("Just born");
+        break;
+    case 18:
+        qInfo("Just starting adulting");
+        break;
+    default:
+        qInfo("whatevess");
+        break;
+    }
     return a.exec();
 }
